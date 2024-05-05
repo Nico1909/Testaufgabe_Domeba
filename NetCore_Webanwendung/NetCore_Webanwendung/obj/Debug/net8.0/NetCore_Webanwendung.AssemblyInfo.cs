10aa726087a32d6ae52182b4b963220f80ed9018
Nico K <nkunz4@hs-mittweida.de>
2024-05-05 22:33:13 +0200

    changes

--- NetCore_Webanwendung/NetCore_Webanwendung/obj/Debug/net8.0/NetCore_Webanwendung.AssemblyInfo.cs
+++ NetCore_Webanwendung/NetCore_Webanwendung/obj/Debug/net8.0/NetCore_Webanwendung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCore_Webanwendung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4e03092d2e7f9b7ec882567aa4744acf196b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCore_Webanwendung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCore_Webanwendung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
